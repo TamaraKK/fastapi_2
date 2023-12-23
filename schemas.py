c1fe91b58b99c2452db5d8cb5087469efa6f685d
TamaraKK <tomkavins@gmail.com>
2023-12-23 15:06:02 +0500

    edit post req

--- schemas.py
+++ schemas.py
@@ -1,11 +1,17 @@
+from typing import List
 from pydantic import BaseModel
 from datetime import date
+
+
+class Genre(BaseModel):
+    name:str
 
 class Book(BaseModel):
     title: str
     author: str
-    genres: str = None
+    genres: List[Genre] = None
     date: date
+    pages: int
 
 
 
